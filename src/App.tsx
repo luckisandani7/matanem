e8b49851a05784d11a83e0a3245ea04c4c836fc9
Fatherless Coder <luckisandani@gmail.com>
2026-04-05 18:16:25 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function App() {
   return (
     <div style={{ backgroundColor: 'black', color: 'white', height: '100vh', display: 'flex', alignItems: 'center', justifyItems: 'center', justifyContent: 'center' }}>
-      <h1 style={{ fontSize: '5rem' }}>HELLO WORLD</h1>
+      <h1 style={{ fontSize: '5rem' }}>HELLO WORLD </h1>
     </div>
   );
 }
